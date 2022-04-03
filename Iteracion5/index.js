1c2e6b9635b64fb5c9b55ba27bcea3b2799d48bc
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-04-03 23:06:03 +0200

    Acabado ejercicio Iteraciones

--- Iteracion5/index.js
+++ Iteracion5/index.js
@@ -1,28 +1,21 @@
-/* Crea una función que reciba por parámetro un array y
- cuando es un valor number lo sume 
- y de lo contrario 
-cuente la longitud del string y lo sume. 
-Puedes usar este array para probar tu función:*/
-
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 
 
 const averageWord = (list) => {
-    let acm = 0;
+    let acc = 0;
   
   list.forEach((element) => {
-    
-    // console.log(element);
+     //console.log(element);
     if(typeof element === 'number'){
         console.log(element);
-        acm += element;
+        acc += element;
      }else {
         console.log(element.length);
-        acm += element.lenght;
+        acc += element.length;
      }
     
 });
-   return acm;
+   return acc;
 
 }
 const result = averageWord(mixedElements);
